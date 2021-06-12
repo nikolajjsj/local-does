a98b3201268800f764f005a5c9ff46614aef736b
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-06-12 16:23:45 +0200

    Simple version done

---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="w-screen h-16 bg-gray-900 text-white sticky top-0">
+      <div className="w-4/5 h-full mx-auto flex items-center">
+        <h1 className="text-xl font-bold">Local list</h1>
+      </div>
+    </nav>
+  );
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,15 @@
 import '../styles/globals.css';
+import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <div suppressHydrationWarning>
+        { process.browser && <Component {...pageProps} /> }
+      </div>
+    </>
+  );
 }
 
 export default MyApp;
