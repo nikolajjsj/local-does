2993a71920560dd73b98b0bd0dbf7d87ff3f17a5
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-06-12 16:54:19 +0200

    Updated logic of index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,28 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { uid } from 'uid';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+const defaultItem = {
+  title: '',
+  id: uid(32),
+  done: false,
+  createdAt: new Date().now,
+};
 
 export default function Home() {
-  const [items, setItems] = useState(
-    () => JSON.parse(window.localStorage.getItem('local-does:items')) || [],
-  );
-  const [currItem, setCurrItem] = useState('');
-
-  useEffect(() => {
-    window.localStorage.setItem('local-does:items', JSON.stringify(items));
-  }, [items]);
+  const [items, setItems] = useLocalStorage();
+  const [currItem, setCurrItem] = useState(defaultItem);
 
   function handleSubmit(event) {
     event.preventDefault();
     setItems((currentItems) => [...currentItems, currItem]);
-    setCurrItem('');
+    setCurrItem(defaultItem);
   }
 
-  function deleteItem(name) {
+  function deleteItem(id) {
     setItems((currItems) => currItems.filter((item) => {
-      if (item === name) return false;
+      if (item.id === id) return false;
       return true;
     }));
   }
@@ -34,24 +37,30 @@ export default function Home() {
       <main className="w-4/5 mx-auto md:w-3/5">
         <form onSubmit={handleSubmit}>
           <input
-            className="w-full my-4 p-2 rounded bg-gray-300 text-black"
+            className="w-full my-4 p-2 rounded bg-gray-300 text-black focus-within:outline-none"
             placeholder="Write another todo"
             id="current-todo"
-            value={currItem}
-            onChange={(event) => setCurrItem(event.target.value)}
+            value={currItem.title}
+            onChange={(event) => setCurrItem({
+              // eslint-disable-next-line new-cap
+              title: event.target.value,
+              id: uid(32),
+              done: false,
+              createdAt: new Date().now,
+            })}
           />
         </form>
 
         {items
           && items.map((item) => (
             <div
-              key={item}
-              className="relative w-full h-32 text-center my-4 pt-4 rounded-lg bg-gray-800 overflow-hidden shadow-md transition"
+              key={item.id}
+              className="flex flex-col cursor-pointer justify-between w-full h-44 text-center my-4 rounded-lg bg-gray-800 overflow-hidden shadow-md transition"
             >
-              <h2 className="text-lg">{item}</h2>
+              <h2 className="w-full h-full mt-4 text-xl">{item.title}</h2>
               <button
-                className="absolute bottom-0 left-0 w-full h-6 bg-red-400 hover:bg-red-500 focus:outline-none"
-                onClick={() => deleteItem(item)}
+                className="w-full h-6 bg-red-400 hover:bg-red-500 focus:outline-none"
+                onClick={() => deleteItem(item.id)}
                 type="button"
               >
                 Delete
