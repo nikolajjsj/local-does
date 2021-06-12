20e2bf54aabba18fa683088162af3d036fc98f65
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2021-06-12 15:13:40 +0200

    Initialized next app

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-props-no-spreading': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
+  },
+};
